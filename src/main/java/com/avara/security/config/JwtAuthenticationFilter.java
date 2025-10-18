b6b6eb5b52b7cf4c7dd574533ce0a9be7b0f65d2
Emre Avara <emre_avara@hotmail.com>
2025-10-18 02:26:02 +0200

    Add token parser service

--- src/main/java/com/avara/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/avara/security/config/JwtAuthenticationFilter.java
@@ -5,14 +5,30 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-  protected void doFilterInternal(HttpServletRequest request,
-                                  HttpServletResponse response,
+  private final JwtService jwtService;
+
+  protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                  @NonNull HttpServletResponse response,
                                   FilterChain filterChain) throws ServletException, IOException {
+    final String authHeader = request.getHeader("Authorization");
+    final String jwtToken;
+    final String userEmail;
+    if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+    jwtToken = authHeader.substring(7);
+    userEmail = jwtService.extractUsername(jwtToken);
 
-
+    filterChain.doFilter(request, response);
   }
 }
